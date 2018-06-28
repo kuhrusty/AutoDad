466688d315099e60898e3df5d638483bea3ac208
Rusty Ballinger <rusty.ballinger@gmail.com>
2018-06-28 02:01:37 -0700

    Remove menu.

--- app/src/main/java/com/kuhrusty/autodad/DisplayAnswerActivity.java
+++ app/src/main/java/com/kuhrusty/autodad/DisplayAnswerActivity.java
@@ -7,7 +7,6 @@ import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -145,27 +144,5 @@ public class DisplayAnswerActivity extends ActionBarActivity {
 ////            am.registerMediaButtonEventReceiver(RemoteControlReceiver);
 //            // Start playback.
 //        }
-    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_display_answer, menu);
-//        return true;
-//    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/kuhrusty/autodad/SoundBoardActivity.java
+++ app/src/main/java/com/kuhrusty/autodad/SoundBoardActivity.java
@@ -5,8 +5,6 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -54,28 +52,6 @@ public class SoundBoardActivity extends ActionBarActivity
             cvg.addView(tb);
         }
 //        setContentView(R.layout.activity_sound_board);
-    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_sound_board, menu);
-//        return true;
-//    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
